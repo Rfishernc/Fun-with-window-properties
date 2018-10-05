5c4cf02c836961827a59e19a2127472916b43e0e
RichFisher <rich.fishernc@gmail.com>
2018-10-05 11:41:58 -0500

    messing around

--- page2.js
+++ page2.js
@@ -1,1 +1,17 @@
-console.log(window.innerHeight);
+function resizeWindow() {
+    document.getElementById('resizer').addEventListener('click', function() {
+        window.resizeBy(300, 300);
+        console.log('hello');
+    })
+}
+
+function fencePos() {
+    document.getElementById('fence').addEventListener('click', function() {
+        let fenceX = event.clientX - document.getElementById(event.target.id).offsetLeft;
+        let fenceY = event.clientY - document.getElementById(event.target.id).offsetTop;
+        console.log(fenceX, fenceY);
+})
+}
+
+resizeWindow();
+fencePos();
